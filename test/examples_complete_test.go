b500cfbe9806792ed5ab463d9a8aceab3594ae16
Justin Oursler <justin.oursler@defenseunicorns.com>
2023-07-27 12:38:23 -0500

    Removing temp testing artifact.

--- test/examples_complete_test.go
+++ test/examples_complete_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestExamplesComplete(t *testing.T) {
 	var approvedRegions = []string{"us-east-1", "us-east-2", "us-west-1", "us-west-2"}
-	awsRegion := "us-west-1" //aws.GetRandomStableRegion(t, approvedRegions, nil)
+	awsRegion := aws.GetRandomStableRegion(t, approvedRegions, nil)
 	backupAwsRegion := aws.GetRandomStableRegion(t, approvedRegions, []string{awsRegion})
 
 	t.Parallel()
